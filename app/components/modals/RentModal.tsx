632643aa2258e64c8bbfea7bb3000dcb5f21e815
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-05-28 11:22:56 -0600

    Update RentModal.tsx

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,27 +1,22 @@
-'use client';
+'use client'; 
 
-import axios from 'axios';
-import { toast } from 'react-hot-toast';
-import { 
-  FieldValues, 
-  SubmitHandler, 
-  useForm
-} from 'react-hook-form';
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/navigation';
-import { useMemo, useState } from "react";
+import useRentModal from '@/app/hooks/useRentModal'; // Importa un hook personalizado para el modal de alquiler
+import Modal from "./Modal"; // Importa el componente Modal
+import { useMemo, useState } from 'react'; // Importa funciones de React
+import Heading from '../Heading'; // Importa el componente Heading
+import { categories } from '../navbar/Categories'; // Importa la lista de categorías desde un archivo local
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'; // Importa funciones y tipos relacionados con react-hook-form
+import CategoryInput from '../inputs/CategoryInput'; // Importa el componente de entrada de categoría
+import CountrySelect from '../inputs/CountrySelect'; // Importa el componente de selección de país
+import dynamic from 'next/dynamic'; // Importa la función dynamic de next/dynamic para la carga dinámica de componente
+import Counter from '../inputs/Counter'; // Importa el componente de contador
+import ImageUpload from '../inputs/ImageUpload'; // Importa el componente de carga de imagen
+import Input from '../inputs/Input'; // Importa el componente de entrada de texto
+import axios from 'axios'; // Importa la librería Axios para hacer solicitudes HTTP
+import toast from 'react-hot-toast'; // Importa la librería de notificaciones toast
+import { useRouter } from 'next/navigation'; // Importa el hook useRouter de next/navigation
 
-import useRentModal from '@/app/hooks/useRentModal';
-
-import Modal from "./Modal";
-import Counter from "../inputs/Counter";
-import CategoryInput from '../inputs/CategoryInput';
-import CountrySelect from "../inputs/CountrySelect";
-import { categories } from '../navbar/Categories';
-import ImageUpload from '../inputs/ImageUpload';
-import Input from '../inputs/Input';
-import Heading from '../Heading';
-
+// Enumeración de pasos del proceso de creación del evento
 enum STEPS {
   CATEGORY = 0,
   LOCATION = 1,
@@ -31,23 +26,23 @@ enum STEPS {
   PRICE = 5,
 }
 
+// Componente RentModal
 const RentModal = () => {
-  const router = useRouter();
-  const rentModal = useRentModal();
+  const router = useRouter(); // Hook useRouter para acceder al enrutador de next.js
+  const rentModal = useRentModal(); // Hook personalizado para el modal de alquiler
 
-  const [isLoading, setIsLoading] = useState(false);
-  const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false); // Estado para indicar si se está cargando
+  const [step, setStep] = useState(STEPS.CATEGORY); // Estado para almacenar el paso actual del proceso de creación del evento
 
+  // Hook useForm para manejar el formulario y sus valores
   const { 
-    register, 
-    handleSubmit,
-    setValue,
-    watch,
-    formState: {
-      errors,
-    },
-    reset,
-  } = useForm<FieldValues>({
+    register, // Función de registro de campos del formulario
+    handleSubmit, // Función para manejar la presentación del formulario
+    setValue, // Función para establecer valores en campos del formulario
+    watch, // Función para observar cambios en los campos del formulario
+    formState: { errors }, // Estado del formulario que contiene los errores
+    reset // Función para restablecer los valores del formulario
+  } = useForm<FieldValues>({ // Configuración del useForm con los valores iniciales del formulario
     defaultValues: {
       category: '',
       location: null,
@@ -61,18 +56,20 @@ const RentModal = () => {
     }
   });
 
-  const location = watch('location');
+  // Variables para observar los valores de los campos del formulario
   const category = watch('category');
+  const location = watch('location');
   const guestCount = watch('guestCount');
   const roomCount = watch('roomCount');
   const bathroomCount = watch('bathroomCount');
   const imageSrc = watch('imageSrc');
 
-  const Map = useMemo(() => dynamic(() => import('../Map'), { 
-    ssr: false 
+  // Componente Map cargado dinámicamente cuando cambia la ubicación
+  const Map = useMemo (() => dynamic(() => import('../Map'),{
+    ssr: false
   }), [location]);
-
-
+  
+  // Función para establecer valores personalizados en campos del formulario
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
       shouldDirty: true,
@@ -81,58 +78,63 @@ const RentModal = () => {
     })
   }
 
+  // Función para retroceder al paso anterior del proceso de creación del evento
   const onBack = () => {
     setStep((value) => value - 1);
-  }
+  };
 
+  // Función para avanzar al siguiente paso del proceso de creación del evento
   const onNext = () => {
     setStep((value) => value + 1);
   }
 
+  // Función para manejar la presentación del formulario
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    // Si no estamos en el último paso, avanzamos al siguiente paso
     if (step !== STEPS.PRICE) {
       return onNext();
     }
-    
-    setIsLoading(true);
 
-    axios.post('/api/listings', data)
-    .then(() => {
-      toast.success('Listing created!');
-      router.refresh();
-      reset();
-      setStep(STEPS.CATEGORY)
-      rentModal.onClose();
+    // Si estamos en el último paso, creamos el listado
+    setIsLoading(true); // Indicamos que se está cargando
+    axios.post('/api/listings', data) // Enviamos los datos al servidor para crear el listado
+    .then(() => { // Si la solicitud es exitosa
+      toast.success('Listing created!'); // Mostramos una notificación de éxito
+      router.refresh(); // Actualizamos la página
+      reset(); // Restablecemos los valores del formulario
+      setStep(STEPS.CATEGORY); // Volvemos al primer paso del proceso
+      rentModal.onClose(); // Cerramos el modal
     })
-    .catch(() => {
-      toast.error('Something went wrong.');
+    .catch(() => { // Si hay un error en la solicitud
+      toast.error('An error occurred.'); // Mostramos una notificación de error
     })
-    .finally(() => {
-      setIsLoading(false);
+    .finally(() => { // Después de que la solicitud termine (ya sea éxito o error)
+      setIsLoading(false); // Indicamos que la carga ha terminado
     })
   }
 
+  // Etiqueta de acción principal del modal (Crear o Siguiente)
   const actionLabel = useMemo(() => {
     if (step === STEPS.PRICE) {
-      return 'Create'
+      return 'Create';
     }
-
-    return 'Next'
+    return 'Next';
   }, [step]);
 
+  // Etiqueta de acción secundaria del modal (Atrás o Ninguna)
   const secondaryActionLabel = useMemo(() => {
     if (step === STEPS.CATEGORY) {
-      return undefined
+      return undefined;
     }
-
-    return 'Back'
+    return 'Back';
   }, [step]);
 
+  // Contenido del cuerpo del modal dependiendo del paso actual del proceso de creación del evento
   let bodyContent = (
     <div className="flex flex-col gap-8">
       <Heading
-        title="Which of these best describes your place?"
-        subtitle="Pick a category"
+        title="¿Cuál de estos describe mejor tu Evento?"
+        subtitle="Elige una categoría"
       />
       <div 
         className="
@@ -159,59 +161,65 @@ const RentModal = () => {
     </div>
   )
 
+  // Renderizado condicional del contenido del cuerpo del modal según el paso actual
   if (step === STEPS.LOCATION) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Where is your place located?"
-          subtitle="Help guests find you!"
+          title="¿Dónde se encuentra tu evento?"
+          subtitle="Ayuda a tus amigos a encontrar tus eventos!"
         />
-        <CountrySelect 
+        <CountrySelect
           value={location} 
           onChange={(value) => setCustomValue('location', value)} 
         />
-        <Map center={location?.latlng} />
+        <Map
+          center={location?.latlng}
+          zoom={15}
+        />        
       </div>
     );
   }
-
+  
   if (step === STEPS.INFO) {
+    // Contenido del cuerpo del modal para el paso de información básica
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Share some basics about your place"
-          subtitle="What amenitis do you have?"
+          title="Comparte algunos aspectos básicos sobre tu evento"
+          subtitle="Cuales son tus requisitos?"
         />
         <Counter 
           onChange={(value) => setCustomValue('guestCount', value)}
           value={guestCount}
-          title="Guests" 
-          subtitle="How many guests do you allow?"
+          title="Participantes" 
+          subtitle="Cuantos participantes permite tu evento"
         />
         <hr />
         <Counter 
           onChange={(value) => setCustomValue('roomCount', value)}
           value={roomCount}
-          title="Rooms" 
-          subtitle="How many rooms do you have?"
+          title="Etapas" 
+          subtitle="¿Cuantas etapas manejara tu evento?"
         />
         <hr />
         <Counter 
           onChange={(value) => setCustomValue('bathroomCount', value)}
           value={bathroomCount}
-          title="Bathrooms" 
-          subtitle="How many bathrooms do you have?"
+          title="Categorias" 
+          subtitle="Cuantas categorias seran permitidas?"
         />
       </div>
     )
   }
 
+  // Renderizado condicional del contenido del cuerpo del modal según el paso actual
   if (step === STEPS.IMAGES) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Add a photo of your place"
-          subtitle="Show guests what your place looks like!"
+          title="Agrega una foto"
+          subtitle="Muestra a los participantes cómo sera tu evento!"
         />
         <ImageUpload
           onChange={(value) => setCustomValue('imageSrc', value)}
@@ -221,25 +229,26 @@ const RentModal = () => {
     )
   }
 
+  // Renderizado condicional del contenido del cuerpo del modal según el paso actual
   if (step === STEPS.DESCRIPTION) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="How would you describe your place?"
-          subtitle="Short and sweet works best!"
+          title="¿Cómo describirías tu evento?"
+          subtitle="¡Divertido y emocionante suena mejor!"
         />
         <Input
           id="title"
-          label="Title"
+          label="Titulo"
           disabled={isLoading}
           register={register}
           errors={errors}
-          required
+          required 
         />
         <hr />
         <Input
           id="description"
-          label="Description"
+          label="Descripcion"
           disabled={isLoading}
           register={register}
           errors={errors}
@@ -249,16 +258,17 @@ const RentModal = () => {
     )
   }
 
+  // Renderizado condicional del contenido del cuerpo del modal según el paso actual
   if (step === STEPS.PRICE) {
     bodyContent = (
       <div className="flex flex-col gap-8">
         <Heading
-          title="Now, set your price"
-          subtitle="How much do you charge per night?"
+          title="Ahora, establece tu precio"
+          subtitle="¿Cuánto se cobra por carrera?"
         />
         <Input
           id="price"
-          label="Price"
+          label="Precio"
           formatPrice 
           type="number" 
           disabled={isLoading}
@@ -270,19 +280,20 @@ const RentModal = () => {
     )
   }
 
+// Renderizado del componente Modal con el contenido del cuerpo correspondiente al paso actual
   return (
     <Modal
-      disabled={isLoading}
-      isOpen={rentModal.isOpen}
-      title="Airbnb your home!"
-      actionLabel={actionLabel}
-      onSubmit={handleSubmit(onSubmit)}
-      secondaryActionLabel={secondaryActionLabel}
-      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-      onClose={rentModal.onClose}
-      body={bodyContent}
+      isOpen={rentModal.isOpen} // Indica si el modal está abierto
+      title="Ruta Veloz tus eventos!" // Título del modal
+      actionLabel={actionLabel} // Etiqueta de la acción principal
+      onSubmit={handleSubmit(onSubmit)} // Función de presentación del formulario
+      secondaryActionLabel={secondaryActionLabel} // Etiqueta de la acción secundaria
+      secondaryAction={step === STEPS.CATEGORY ? undefined : onBack} // Función de la acción secundaria
+      onClose={rentModal.onClose} // Función de cierre del modal
+      body={bodyContent} // Contenido del cuerpo del modal
     />
   );
 }
 
-export default RentModal;
+// Exporta el componente RentModal
+export default RentModal; 
