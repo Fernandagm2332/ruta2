f9f5e54c112e17658bd017cf48e611f6ab866eb5
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-05-30 00:15:04 -0600

    Update page.tsx

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -15,8 +15,8 @@ const ListingPage = async () => {
     return (
       <ClientOnly>
         <EmptyState
-          title="No favorites found"
-          subtitle="Looks like you have no favorite listings."
+          title="No se han encontrado favoritos"
+          subtitle="Parece que no tienes eventos favoritos."
         />
       </ClientOnly>
     );
