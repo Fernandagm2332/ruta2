8bc6c9128e2880a72767374d7eaab57da8dac317
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 16:30:02 -0600

    Update getFavoriteListings.ts

--- app/actions/getFavoriteListings.ts
+++ app/actions/getFavoriteListings.ts
@@ -1,30 +1,35 @@
-import prisma from "@/app/libs/prismadb";
+// modulo es para definir una función para obtener los listados favoritos del usuario 
 
-import getCurrentUser from "./getCurrentUser";
+import prisma from "@/app/libs/prismadb"; // Importa la instancia de Prisma para interactuar con la base de datos
+import getCurrentUser from "./getCurrentUser"; // Importa la función para obtener el usuario actual
 
 export default async function getFavoriteListings() {
   try {
-    const currentUser = await getCurrentUser();
+    const currentUser = await getCurrentUser(); // Obtiene el usuario actual
 
+    // Si no hay un usuario actual retorna un arreglo vacio
     if (!currentUser) {
       return [];
     }
 
+    // Busca los listados favoritos del usuario en la base de datos
     const favorites = await prisma.listing.findMany({
       where: {
         id: {
-          in: [...(currentUser.favoriteIds || [])]
-        }
-      }
+          in: [...(currentUser.favoriteIds || [])], // Utiliza los ID de los favoritos del usuario
+        },
+      },
     });
 
+    // Mapea los listados favoritos para convertir las fechas a strings
     const safeFavorites = favorites.map((favorite) => ({
       ...favorite,
-      createdAt: favorite.createdAt.toString(),
+      createdAt: favorite.createdAt.toString(), // Convierte createdAt a string
     }));
 
-    return safeFavorites;
+    return safeFavorites; // Retorna los listados favoritos
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error(error); // Lanza un error en caso de que ocurra alguno
   }
 }
+
