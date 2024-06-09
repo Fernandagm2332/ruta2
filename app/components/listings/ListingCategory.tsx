05ebca8665cf17d1f1cd739b2de45fca84864d46
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 17:13:27 -0600

    Update ListingCategory.tsx

--- app/components/listings/ListingCategory.tsx
+++ app/components/listings/ListingCategory.tsx
@@ -1,13 +1,17 @@
 'use client';
+// Este módulo define un componente de vista de categoría utilizado para mostrar iconos, etiquetas y descripciones de categorías
 
+// Importa el tipo de icono de React
 import { IconType } from "react-icons";
 
+// Interfaz para las propiedades del componente CategoryView
 interface CategoryViewProps {
-  icon: IconType,
-  label: string,
-  description: string
+  icon: IconType; // Icono de la categoría
+  label: string; // Etiqueta de la categoría
+  description: string; // Descripción de la categoría
 }
 
+// Componente funcional CategoryView
 const CategoryView: React.FC<CategoryViewProps> = ({ 
   icon: Icon,
   label,
@@ -16,15 +20,15 @@ const CategoryView: React.FC<CategoryViewProps> = ({
   return ( 
     <div className="flex flex-col gap-6">
       <div className="flex flex-row items-center gap-4">
-        <Icon size={40} className="text-neutral-600" />
+        <Icon size={40} className="text-neutral-600" /> {/* Icono con tamaño específico */}
         <div className="flex flex-col">
             <div 
-              className="text-lg font-semibold"
+              className="text-lg font-semibold" // Etiqueta de categoría con fuente más gruesa
             >
               {label}
             </div>
             <div 
-              className="text-neutral-500 font-light"
+              className="text-neutral-500 font-light" // Descripción de categoría con fuente más ligera
             >
               {description}
             </div>
@@ -34,4 +38,4 @@ const CategoryView: React.FC<CategoryViewProps> = ({
    );
 }
  
-export default CategoryView;
+export default CategoryView; // Exporta el componente CategoryView
