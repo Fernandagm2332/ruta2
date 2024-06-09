75ffd15192e22fdc6cc92634c3d1367abbfc121f
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 17:00:58 -0600

    Update Calendar.tsx

--- app/components/inputs/Calendar.tsx
+++ app/components/inputs/Calendar.tsx
@@ -1,20 +1,24 @@
+// Este módulo define un componente de selector de fechas utilizando la librería React-Date-Range.
 'use client';
-
+// Importa los componentes necesarios de React-Date-Range
 import { 
   DateRange, 
   Range, 
   RangeKeyDict
 } from 'react-date-range';
 
+// Importa los estilos predeterminados de React-Date-Range
 import 'react-date-range/dist/styles.css';
 import 'react-date-range/dist/theme/default.css';
 
+// Interfaz para las propiedades del componente DatePicker
 interface DatePickerProps {
-  value: Range,
-  onChange: (value: RangeKeyDict) => void;
-  disabledDates?: Date[];
+  value: Range, // Valor del rango de fechas seleccionado
+  onChange: (value: RangeKeyDict) => void; // Función de cambio de valor del rango de fechas
+  disabledDates?: Date[]; // Fechas deshabilitadas
 }
 
+// Componente funcional DatePicker
 const DatePicker: React.FC<DatePickerProps> = ({
   value,
   onChange,
@@ -22,16 +26,16 @@ const DatePicker: React.FC<DatePickerProps> = ({
 }) => {
   return ( 
     <DateRange
-      rangeColors={['#262626']}
-      ranges={[value]}
-      date={new Date()}
-      onChange={onChange}
-      direction="vertical"
-      showDateDisplay={false}
-      minDate={new Date()}
-      disabledDates={disabledDates}
+      rangeColors={['#262626']} // Color del rango de fechas seleccionado
+      ranges={[value]} // Rangos de fechas seleccionados
+      date={new Date()} // Fecha actual
+      onChange={onChange} // Función de cambio de valor del rango de fechas
+      direction="vertical" // Dirección del selector de fechas (vertical)
+      showDateDisplay={false} // Oculta la visualización de la fecha seleccionada
+      minDate={new Date()} // Fecha mínima seleccionable (actual)
+      disabledDates={disabledDates} // Fechas deshabilitadas
     />
    );
 }
  
-export default DatePicker;
+export default DatePicker; // Exporta el componente DatePicker
