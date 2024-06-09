8d4d6f572dc7d3046d8d9df4f7e7fc87882b9d73
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 16:56:11 -0600

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,20 +1,24 @@
-import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+// Este modulo es para definir una función para manejar solicitudes POST relacionadas con el registro de nuevos usuarios 
 
-import prisma from "@/app/libs/prismadb";
+import { NextResponse } from "next/server"; 
+import bcrypt from "bcrypt"; // Importa bcrypt para el hashing de contraseñas
 
+import prisma from "@/app/libs/prismadb"; // Importa la instancia de Prisma para interactuar con la base de datos
+
+// Función asincrónica para manejar solicitudes POST
 export async function POST(
-  request: Request, 
+  request: Request,
 ) {
-  const body = await request.json();
+  const body = await request.json(); // Obtiene el cuerpo de la solicitud en formato JSON
   const { 
     email,
     name,
     password,
-   } = body;
+   } = body; // Extrae los datos del cuerpo de la solicitud
 
-   const hashedPassword = await bcrypt.hash(password, 12);
+   const hashedPassword = await bcrypt.hash(password, 12); // Genera un hash para la contraseña proporcionada
 
+   // Crea un nuevo usuario en la base de datos utilizando los datos proporcionados y la contraseña hasheada
    const user = await prisma.user.create({
     data: {
       email,
@@ -23,5 +27,6 @@ export async function POST(
     }
   });
 
-  return NextResponse.json(user);
+  return NextResponse.json(user); // Retorna la respuesta en formato JSON con los datos del nuevo usuario
 }
+
