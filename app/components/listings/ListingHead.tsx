5e48389b3a7314b4d18c79650eaf841b077f5098
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 17:15:08 -0600

    Update ListingHead.tsx

--- app/components/listings/ListingHead.tsx
+++ app/components/listings/ListingHead.tsx
@@ -1,21 +1,25 @@
 'use client';
+// Este módulo define un componente de cabecera de listado o listingHead, utilizado para mostrar el título, la ubicación y la imagen del listado.
 
+// Importa el componente Image de Next.js y el hook useCountries y los tipos de datos SafeUser
 import Image from "next/image";
-
 import useCountries from "@/app/hooks/useCountries";
 import { SafeUser } from "@/app/types";
 
+// Importa el componente Heading y el componente HeartButton
 import Heading from "../Heading";
 import HeartButton from "../HeartButton";
 
+// Interfaz para las propiedades del componente ListingHead
 interface ListingHeadProps {
-  title: string;
-  locationValue: string;
-  imageSrc: string;
-  id: string;
-  currentUser?: SafeUser | null
+  title: string; // Título del listado
+  locationValue: string; // Valor de la ubicación del listado
+  imageSrc: string; // Fuente de la imagen del listado
+  id: string; // Identificador del listado
+  currentUser?: SafeUser | null; // Usuario actual (opcional)
 }
 
+// Componente funcional ListingHead
 const ListingHead: React.FC<ListingHeadProps> = ({
   title,
   locationValue,
@@ -23,15 +27,15 @@ const ListingHead: React.FC<ListingHeadProps> = ({
   id,
   currentUser
 }) => {
-  const { getByValue } = useCountries();
+  const { getByValue } = useCountries(); // Función para obtener país por valor
 
-  const location = getByValue(locationValue);
+  const location = getByValue(locationValue); // Ubicación del listado
 
   return ( 
     <>
       <Heading
         title={title}
-        subtitle={`${location?.region}, ${location?.label}`}
+        subtitle={`${location?.region}, ${location?.label}`} // Subtítulo con la región y la etiqueta de ubicación
       />
       <div className="
           w-full
@@ -64,4 +68,4 @@ const ListingHead: React.FC<ListingHeadProps> = ({
    );
 }
  
-export default ListingHead;
+export default ListingHead; // Exporta el componente ListingHead
