09832e2dbc7a62ee5fdcf0933d2753b624365048
JonathanGpe004 <152316742+JonathanGpe004@users.noreply.github.com>
2024-06-09 16:41:55 -0600

    Update getReservations.ts

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -1,5 +1,8 @@
-import prisma from "@/app/libs/prismadb";
+// modulo que define una funcion para obtener reservas basadas en varios parametros de busqueda 
 
+import prisma from "@/app/libs/prismadb"; // Importa la instancia de Prisma para conectaar con la base de datos
+
+// Interfaz para los parametros de búsqueda de reservas
 interface IParams {
   listingId?: string;
   userId?: string;
@@ -10,46 +13,53 @@ export default async function getReservations(
   params: IParams
 ) {
   try {
-    const { listingId, userId, authorId } = params;
+    const { listingId, userId, authorId } = params; // extrae los parametros de búsqueda
 
-    const query: any = {};
+    const query: any = {}; // objeto para construir la consulta
         
+    // agrega el listingId a la consulta si esta presente
     if (listingId) {
       query.listingId = listingId;
-    };
+    }
 
+    // agrega el userId a la consulta si esta presente
     if (userId) {
       query.userId = userId;
     }
 
+    // Agrega el authorId a la consulta si esta presente
     if (authorId) {
       query.listing = { userId: authorId };
     }
 
+    // Busca las reservas en la base de datos
     const reservations = await prisma.reservation.findMany({
       where: query,
       include: {
-        listing: true
+        listing: true, // Incluye los detalles del listado asociado a la reserva
       },
       orderBy: {
-        createdAt: 'desc'
+        createdAt: 'desc', // Ordena los resultados por fecha de creación descendente
       }
     });
 
+    // Mapea las reservas para convertir las fechas a ISO string
     const safeReservations = reservations.map(
       (reservation) => ({
-      ...reservation,
-      createdAt: reservation.createdAt.toISOString(),
-      startDate: reservation.startDate.toISOString(),
-      endDate: reservation.endDate.toISOString(),
-      listing: {
-        ...reservation.listing,
-        createdAt: reservation.listing.createdAt.toISOString(),
-      },
-    }));
+        ...reservation,
+        createdAt: reservation.createdAt.toISOString(), // Convierte createdAt a ISO string
+        startDate: reservation.startDate.toISOString(), // Convierte startDate a ISO string
+        endDate: reservation.endDate.toISOString(), // Convierte endDate a ISO string
+        listing: {
+          ...reservation.listing,
+          createdAt: reservation.listing.createdAt.toISOString(), // Convierte createdAt del listado a ISO string
+        },
+      })
+    );
 
-    return safeReservations;
+    return safeReservations; // Retorna las reservas seguras
   } catch (error: any) {
-    throw new Error(error);
+    throw new Error(error); // Lanza un error en caso de que ocurra alguno
   }
 }
+
