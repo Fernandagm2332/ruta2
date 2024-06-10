449713f053f8e7b263cd6026148b96184c432cae
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 00:31:15 -0600

    Update Navbar.tsx

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -48,4 +48,59 @@ const Navbar: React.FC <NavbarProps> =({
   );
 }
 
+export default Navbar;'use client'; // Indica que este archivo se ejecutará en el cliente (navegador)
+
+// Importaciones
+import { User } from "@prisma/client"; // Importa la interfaz User del cliente Prisma
+import Container from "../Container"; // Importa el componente Container
+import Logo from "./Logo"; // Importa el componente Logo
+import Search from "./Search"; // Importa el componente Search
+import UserMenu from "./UserMenu"; // Importa el componente UserMenu
+import { SafeUser } from "@/app/types"; // Importa la interfaz SafeUser de los tipos de la aplicación
+import Categories from "./Categories"; // Importa el componente Categories
+
+// Definición de la interfaz TypeScript para las props del componente Navbar
+interface NavbarProps {
+  currentUser?: SafeUser | null; // Propiedad opcional currentUser de tipo SafeUser o null
+}
+
+// Definición del componente funcional Navbar con React y TypeScript
+const Navbar: React.FC<NavbarProps> = ({
+  currentUser // Desestructuración de la prop currentUser
+}) => {
+  console.log({currentUser}); // Registro en consola del usuario actual
+
+  // Retorno del componente JSX
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      <div
+        className="
+          py=4
+          border-b-[1px]
+        "
+      >
+        <Container>
+          <div 
+            className="
+              flex 
+              flex-row 
+              items-center 
+              justify-between
+              gap-3
+              md:gap-0
+            "
+          >
+            <Logo /> {/* Componente del logo */}
+            <Search /> {/* Componente de búsqueda */}
+            <UserMenu currentUser={currentUser} /> {/* Componente del menú de usuario con el usuario actual como prop */}
+          </div>
+        </Container>
+      </div> 
+      <Categories /> {/* Componente de categorías */}
+    </div>
+  );
+}
+
+// Exporta el componente Navbar para que pueda ser utilizado en otras partes de la aplicación
 export default Navbar;
+
