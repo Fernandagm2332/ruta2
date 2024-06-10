198258079940cdad6640d8f53c0b101c7501e890
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 11:36:03 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,24 @@
+import { Nunito } from 'next/font/google'
 
-//Este archivo configura el diseño raíz de la aplicación, incluyendo la importación de estilos globales, 
-//componentes modales para registro, inicio de sesión, alquiler y búsqueda, así como el proveedor de tostadas para notificaciones
-//También establece la fuente global y obtiene el usuario actual para pasarlo a la barra de navegación
-import {Nunito} from "next/font/google";
+import Navbar from '@/app/components/navbar/Navbar';
+import LoginModal from '@/app/components/modals/LoginModal';
+import RegisterModal from '@/app/components/modals/RegisterModal';
+import SearchModal from '@/app/components/modals/SearchModal';
+import RentModal from '@/app/components/modals/RentModal';
 
-import "./globals.css";
-import Navbar from "./components/navbar/Navbar";
-import ClientOnly from "./components/ClientOnly";
-import RegisterModal from "./components/modals/RegisterModal";
-import LoginModal from "./components/modals/LoginModal";
-import RentModal from "./components/modals/RentModal";
-import SearchModal from "./components/modals/SearchModal";
-import ToasterProvider from "./providers/ToasterProvider";
-import getCurrentUser from "./actions/getCurrentUser";
+import ToasterProvider from '@/app/providers/ToasterProvider';
 
+import './globals.css'
+import ClientOnly from './components/ClientOnly';
+import getCurrentUser from './actions/getCurrentUser';
 
-
-export const metadata= {
-  title: "Vuelta Veloz",
-  description: "Vuelta  veloz!",
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
 }
 
-const font = Nunito({
-  subsets : ["latin"]
+const font = Nunito({ 
+  subsets: ['latin'], 
 });
 
 export default async function RootLayout({
@@ -34,19 +30,20 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className= {font.className}>
-        <ClientOnly> 
+      <body className={font.className}>
+        <ClientOnly>
           <ToasterProvider />
-          <SearchModal/>
-          <RentModal/> 
-          <LoginModal/> 
-          <RegisterModal/>
-          <Navbar currentUser = {currentUser}/>
+          <LoginModal />
+          <RegisterModal />
+          <SearchModal />
+          <RentModal />
+          <Navbar currentUser={currentUser} />
         </ClientOnly>
         <div className="pb-20 pt-28">
           {children}
         </div>
-        </body>
+      </body>
     </html>
   )
 }
+
