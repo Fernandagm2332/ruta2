d811235573e7a30ff7b8e79ae2e6843821604704
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 11:07:10 -0600

    Update CategoryBox.tsx

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -1,59 +1,51 @@
 'use client';
 
-import qs from 'query-string'; // Importa la biblioteca query-string para manejar las URLs y las query strings.
-import { useRouter, useSearchParams } from "next/navigation"; // Importa hooks de Next.js para la navegación y manejo de parámetros de búsqueda.
-import { useCallback } from "react"; // Importa el hook useCallback de React para memoizar funciones.
-import { IconType } from "react-icons"; // Importa el tipo IconType de react-icons para definir el tipo de icono que se usará en el componente.
+import qs from 'query-string';
+import { useRouter, useSearchParams } from "next/navigation";
+import { useCallback } from "react";
+import { IconType } from "react-icons";
 
-// Interfaz para las propiedades del componente CategoryBox
 interface CategoryBoxProps {
-  icon: IconType; // Tipo de icono a mostrar.
-  label: string; // Etiqueta de la categoría.
-  selected?: boolean; // Indica si la categoría está seleccionada (opcional).
+  icon: IconType,
+  label: string;
+  selected?: boolean;
 }
 
-// Definición del componente CategoryBox
 const CategoryBox: React.FC<CategoryBoxProps> = ({
   icon: Icon,
   label,
   selected,
 }) => {
-  const router = useRouter(); // Hook para manejar la navegación.
-  const params = useSearchParams(); // Hook para obtener los parámetros de búsqueda actuales.
+  const router = useRouter();
+  const params = useSearchParams();
 
-  // Función que maneja el clic en la caja de categoría
   const handleClick = useCallback(() => {
     let currentQuery = {};
     
-    // Si hay parámetros de búsqueda actuales, los parsea a un objeto
     if (params) {
-      currentQuery = qs.parse(params.toString());
+      currentQuery = qs.parse(params.toString())
     }
 
-    // Actualiza la query con la categoría seleccionada
     const updatedQuery: any = {
       ...currentQuery,
       category: label
-    };
+    }
 
-    // Si la categoría seleccionada ya está en los parámetros, la elimina
     if (params?.get('category') === label) {
       delete updatedQuery.category;
     }
 
-    // Construye la nueva URL con los parámetros actualizados
     const url = qs.stringifyUrl({
       url: '/',
       query: updatedQuery
     }, { skipNull: true });
 
-    // Navega a la nueva URL
     router.push(url);
   }, [label, router, params]);
 
   return ( 
     <div
-      onClick={handleClick} // Llama a handleClick cuando se hace clic en la caja
+      onClick={handleClick}
       className={`
         flex 
         flex-col 
@@ -65,17 +57,16 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
         hover:text-neutral-800
         transition
         cursor-pointer
-        ${selected ? 'border-b-neutral-800' : 'border-transparent'} // Estilo condicional para la frontera inferior
-        ${selected ? 'text-neutral-800' : 'text-neutral-500'} // Estilo condicional para el color del texto
+        ${selected ? 'border-b-neutral-800' : 'border-transparent'}
+        ${selected ? 'text-neutral-800' : 'text-neutral-500'}
       `}
     >
-      <Icon size={26} /> {/* Muestra el icono con un tamaño de 26 */}
+      <Icon size={26} />
       <div className="font-medium text-sm">
-        {label} {/* Muestra la etiqueta de la categoría */}
+        {label}
       </div>
     </div>
-  );
+   );
 }
  
 export default CategoryBox;
-
