b34de29e61bde2ec5cc296192ff9e130ef8ffd7a
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 10:39:48 -0600

    Update HeartButton.tsx

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,21 +1,23 @@
 'use client';
 
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"; // Importa los iconos de corazón de react-icons.
 
-import { SafeUser } from "@/app/types";
+import { SafeUser } from "@/app/types"; // Importa el tipo SafeUser desde el proyecto.
 
-import ClientOnly from "./ClientOnly";
-import useFavorite from "../hooks/useFavorite";
+import ClientOnly from "./ClientOnly"; // Importa el componente ClientOnly.
+import useFavorite from "../hooks/useFavorite"; // Importa el hook personalizado useFavorite.
 
 interface HeartButtonProps {
-  listingId: string
-  currentUser?: SafeUser | null;
+  listingId: string; // ID del listado al que se va a asociar el botón de corazón.
+  currentUser?: SafeUser | null; // Información del usuario actual, opcional.
 }
 
+// Define el componente HeartButton, que acepta HeartButtonProps
 const HeartButton: React.FC<HeartButtonProps> = ({ 
   listingId,
   currentUser
 }) => {
+  // Utiliza el hook useFavorite para determinar si el listado ha sido marcado como favorito y para manejar el cambio de estado de favorito.
   const { hasFavorited, toggleFavorite } = useFavorite({
     listingId,
     currentUser
@@ -23,7 +25,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
 
   return (
     <div 
-      onClick={toggleFavorite}
+      onClick={toggleFavorite} // Llama a toggleFavorite cuando se hace clic en el botón
       className="
         relative
         hover:opacity-80
@@ -31,6 +33,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         cursor-pointer
       "
     >
+      {/* Icono de corazón vacío que se muestra detrás del corazón lleno */}
       <AiOutlineHeart
         size={28}
         className="
@@ -40,6 +43,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
           -right-[2px]
         "
       />
+      {/* Icono de corazón lleno que cambia de color dependiendo si está marcado como favorito */}
       <AiFillHeart
         size={24}
         className={
@@ -47,7 +51,7 @@ const HeartButton: React.FC<HeartButtonProps> = ({
         }
       />
     </div>
-   );
+  );
 }
  
 export default HeartButton;
