8f8a6e6b391f366e206937d9286a8dbae0f42fb3
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 11:04:27 -0600

    Update ClientOnly.tsx

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -1,26 +1,22 @@
 'use client';
 
-import React, { useState, useEffect } from 'react'; // Importa React y los hooks useState y useEffect.
+import React, { useState, useEffect } from 'react';
 
 interface ClientOnlyProps {
-  children: React.ReactNode; // Define la propiedad children que representa los elementos secundarios que el componente envolverá.
+  children: React.ReactNode;
 }
 
-// Define el componente funcional ClientOnly que acepta ClientOnlyProps.
 const ClientOnly: React.FC<ClientOnlyProps> = ({ 
   children
 }) => {
-  const [hasMounted, setHasMounted] = useState(false); // Estado para rastrear si el componente se ha montado en el cliente.
+  const [hasMounted, setHasMounted] = useState(false);
 
-  // useEffect para establecer el estado hasMounted a true después de que el componente se haya montado.
   useEffect(() => {
       setHasMounted(true);
-  }, []) // El array vacío asegura que el efecto solo se ejecute una vez, después del primer renderizado.
+  }, [])
 
-  // Si el componente aún no se ha montado, retorna null y no renderiza nada.
   if (!hasMounted) return null;
 
-  // Una vez montado, renderiza los children.
   return (
     <>
       {children}
@@ -28,5 +24,4 @@ const ClientOnly: React.FC<ClientOnlyProps> = ({
   );
 };
 
-export default ClientOnly; // Exporta el componente ClientOnly como el valor predeterminado del módulo.
-
+export default ClientOnly;
