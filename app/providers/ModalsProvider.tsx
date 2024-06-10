e2a8cfd19cd4cac15622b5f48f37a0856785e812
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 10:55:41 -0600

    Update ModalsProvider.tsx

--- app/providers/ModalsProvider.tsx
+++ app/providers/ModalsProvider.tsx
@@ -1,19 +1,20 @@
 'use client';
 
-import LoginModal from "../components/modals/LoginModal";
-import RegisterModal from "../components/modals/RegisterModal";
-import RentModal from "../components/modals/RentModal";
-import SearchModal from "../components/modals/SearchModal";
+import LoginModal from "../components/modals/LoginModal"; // Importa el componente LoginModal desde la carpeta de componentes.
+import RegisterModal from "../components/modals/RegisterModal"; // Importa el componente RegisterModal desde la carpeta de componentes.
+import RentModal from "../components/modals/RentModal"; // Importa el componente RentModal desde la carpeta de componentes.
+import SearchModal from "../components/modals/SearchModal"; // Importa el componente SearchModal desde la carpeta de componentes.
 
+// Define el componente funcional ModalsProvider
 const ModalsProvider = () => {
   return ( 
     <>
-      <LoginModal />
-      <RegisterModal />
-      <SearchModal />
-      <RentModal />
+      <LoginModal />     {/* Renderiza el modal de inicio de sesión */}
+      <RegisterModal />  {/* Renderiza el modal de registro */}
+      <SearchModal />    {/* Renderiza el modal de búsqueda */}
+      <RentModal />      {/* Renderiza el modal de alquiler */}
     </>
-   );
+  );
 }
- 
-export default ModalsProvider;
+
+export default ModalsProvider; // Exporta el componente ModalsProvider como el valor predeterminado del módulo.
