cf2649d3b65c81fa379d50ae73cd0dd2cfc9cbcb
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 11:03:09 -0600

    Update EmptyState.tsx

--- app/components/EmptyState.tsx
+++ app/components/EmptyState.tsx
@@ -1,23 +1,22 @@
 'use client';
 
-import { useRouter } from "next/navigation"; // Importa el hook useRouter de Next.js para la navegación.
+import { useRouter } from "next/navigation";
 
-import Button from "./Button"; // Importa el componente Button desde el proyecto.
-import Heading from "./Heading"; // Importa el componente Heading desde el proyecto.
+import Button from "./Button";
+import Heading from "./Heading";
 
 interface EmptyStateProps {
-  title?: string; // Título opcional para mostrar en el estado vacío.
-  subtitle?: string; // Subtítulo opcional para mostrar en el estado vacío.
-  showReset?: boolean; // Booleano opcional para mostrar o no el botón de reset.
+  title?: string;
+  subtitle?: string;
+  showReset?: boolean;
 }
 
-// Define el componente funcional EmptyState que acepta EmptyStateProps.
 const EmptyState: React.FC<EmptyStateProps> = ({
-  title = "No hay coincidencias exactas", // Valor por defecto para el título.
-  subtitle = "Intente cambiar o quitar algunos de sus filtros", // Valor por defecto para el subtítulo.
-  showReset // Propiedad opcional para mostrar el botón de reset.
+  title = "No hay coincidencias exactas",
+  subtitle = "Intente cambiar o quitar algunos de sus filtros",
+  showReset
 }) => {
-  const router = useRouter(); // Obtiene la instancia del router para manejar la navegación.
+  const router = useRouter();
 
   return ( 
     <div 
@@ -32,20 +31,20 @@ const EmptyState: React.FC<EmptyStateProps> = ({
     >
       <Heading
         center
-        title={title} // Pasa el título al componente Heading.
-        subtitle={subtitle} // Pasa el subtítulo al componente Heading.
+        title={title}
+        subtitle={subtitle}
       />
       <div className="w-48 mt-4">
         {showReset && (
           <Button
             outline
-            label="Eliminar todos los filtros" // Etiqueta del botón.
-            onClick={() => router.push('/')} // Navega a la página de inicio al hacer clic.
+            label="Eliminar todos los filtros"
+            onClick={() => router.push('/')}
           />
         )}
       </div>
     </div>
-  );
+   );
 }
-
-export default EmptyState; // Exporta el componente EmptyState como el valor predeterminado del módulo.
+ 
+export default EmptyState; 
