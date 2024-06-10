eedeaf6828318865a4ce4352810ff5c38c330c74
Odalys Fernanda Gomez Alviso <150950223+Fernandagm2332@users.noreply.github.com>
2024-06-10 11:06:38 -0600

    Update Map.tsx

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,13 +1,13 @@
-'use client';
+  'use client';
 
-import L from 'leaflet'; // Importa la biblioteca Leaflet para mapas interactivos.
-import { MapContainer, Marker, TileLayer } from 'react-leaflet'; // Importa componentes específicos de react-leaflet para crear y gestionar mapas.
-import 'leaflet/dist/leaflet.css'; // Importa los estilos CSS necesarios para Leaflet.
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'; // Importa el icono de marcador de alta resolución.
-import markerIcon from 'leaflet/dist/images/marker-icon.png'; // Importa el icono de marcador estándar.
-import markerShadow from 'leaflet/dist/images/marker-shadow.png'; // Importa la sombra del icono del marcador.
+import L from 'leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet'
 
-// Configura los iconos de Leaflet para que utilicen los archivos importados.
+import 'leaflet/dist/leaflet.css'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
 // @ts-ignore
 delete L.Icon.Default.prototype._getIconUrl; 
 L.Icon.Default.mergeOptions({
@@ -16,32 +16,30 @@ L.Icon.Default.mergeOptions({
     shadowUrl: markerShadow.src,
 });
 
-// Interfaz para las propiedades del componente Map
 interface MapProps {
-  center?: number[]; // Coordenadas opcionales para centrar el mapa
+  center?: number[]
 }
 
-const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"; // URL de los tiles de OpenStreetMap
-const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'; // Atribución requerida por OpenStreetMap
+const url = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
-// Definición del componente Map
 const Map: React.FC<MapProps> = ({ center }) => {
   return (
-    <MapContainer 
-      center={center as L.LatLngExpression || [51, -0.09]} // Centra el mapa en las coordenadas proporcionadas o en [51, -0.09] por defecto
-      zoom={center ? 4 : 2} // Define el nivel de zoom dependiendo de si se proporcionaron coordenadas
-      scrollWheelZoom={false} // Desactiva el zoom con la rueda del ratón
-      className="h-[35vh] rounded-lg" // Aplica clases de Tailwind CSS para la altura y el borde del mapa
-    >
-      <TileLayer
-        url={url} // URL de los tiles de OpenStreetMap
-        attribution={attribution} // Atribución de OpenStreetMap
-      />
-      {center && (
-        <Marker position={center as L.LatLngExpression} /> // Si se proporcionan coordenadas, añade un marcador en esa posición
-      )}
-    </MapContainer>
-  );
+      <MapContainer 
+        center={center as L.LatLngExpression || [51, -0.09]} 
+        zoom={center ? 4 : 2} 
+        scrollWheelZoom={false} 
+        className="h-[35vh] rounded-lg"
+      >
+        <TileLayer
+          url={url}
+          attribution={attribution}
+        />
+        {center && (
+          <Marker position={center as L.LatLngExpression} />
+        )}
+      </MapContainer>
+  )
 }
 
 export default Map;
